55333f127935e7ff3b61b487775829bc7a077f04
Alina Moskieva <alinamoskieva@gmail.com>
2024-02-09 20:18:06 +0100

    Wrap counter, add title

--- src/ShowCounter.tsx
+++ src/ShowCounter.tsx
@@ -2,13 +2,27 @@ import React from "react";
 import styled from "styled-components";
 import DateDisplay from "./DateDisplay";
 
-const CounterWrapper = styled.div`
+const DateDisplayWrapper = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
   padding: 0.5rem;
-  height: 100vh;
+  height: 50vh;
+  
+`;
+
+const CounterWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  order: 1;
+`;
+
+const Title = styled.h1`
+  align-self: center;
+  padding-top: 25vh;
+  font-size: 2.5rem;
+  color: #ff8fab;
 `;
 
 type ShowCounterProps = {
@@ -21,10 +35,13 @@ type ShowCounterProps = {
 const ShowCounter: React.FC<ShowCounterProps> = ({days, hours, minutes, seconds}) => {
   return(
     <CounterWrapper>
-      <DateDisplay value={days} type="Days"/>
-      <DateDisplay value={hours} type="Hours"/>
-      <DateDisplay value={minutes} type="Minutes"/>
-      <DateDisplay value={seconds} type="Seconds"/>
+      <Title>Your card will be available in</Title>
+      <DateDisplayWrapper>
+        <DateDisplay value={days} type="Days"/>
+        <DateDisplay value={hours} type="Hours"/>
+        <DateDisplay value={minutes} type="Minutes"/>
+        <DateDisplay value={seconds} type="Seconds"/>
+      </DateDisplayWrapper>
     </CounterWrapper>
   )
 }
